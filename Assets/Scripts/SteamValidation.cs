af9002ee8bc7983984392ceb3bfefc669740f5d5
Joseph-Alan-Shelton <josephalashelton@gmail.com>
2025-04-03 11:51:44 -0600

    Added Steam Works folder and implimented steam validation

---
+++ Assets/Scripts/SteamValidation.cs
@@ -0,0 +1,33 @@
+/**
+This class will act as the controller for the steam authorization. A user must have their steam engine open for this to work. This class
+will utilize Steam Works, whos script can be found in the Steamworks.NET folder under SteamManager.cs
+*/
+using Steamworks;
+using UnityEngine;
+
+public class SteamValidation : MonoBehaviour
+{
+    //Variables that will hold the users Steam Name and Steam ID
+    public static ulong steamID{ get; private set;}
+    public static string steamProfileName { get; private set;}
+
+    /// <summary>
+    /// On the initilization of the app this method will look for the steam manager and then retreive user information
+    /// </summary>
+    private void Start()
+    {
+        //Checks if the steam manager is open. If it is not it will simply return
+        if(!SteamManager.Initialized){
+            return; //Ensure steam is open
+        }
+
+        //Retreive user information
+        steamProfileName = SteamFriends.GetPersonaName();
+        CSteamID csteamID = SteamUser.GetSteamID();
+        steamID = csteamID.m_SteamID;
+
+        //Sanity print to ensure it is working
+        Debug.Log("Logged in user is: " + steamProfileName + ", and you Steam ID is: " + steamID);
+    }
+
+}
