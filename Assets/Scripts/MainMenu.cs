51bfc8ee15ad2c68f8707206a2e30d16319b5630
drewiscoo42 <drewiscoo42@gmail.com>
2025-04-17 20:13:12 -0600

    added changes from UIdesigntest branch

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitApp() {
+        Application.Quit();
+        Debug.Log("app quit");
+    }
+}
