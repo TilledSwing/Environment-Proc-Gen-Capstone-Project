72c7216ac8aab410065364e4e720b02b621d7808
Joseph-Alan-Shelton <josephalashelton@gmail.com>
2025-09-06 15:41:19 -0600

    Asset script

---
+++ sqlconnect/saveAssets.php
@@ -0,0 +1,73 @@
+<?php
+    header('Content-Type: application/json');
+    $host = "maria.eng.utah.edu";
+    $dbname = "pegg";
+    $username = "u1431093";
+    $password = "wV8MzCJHN04lsDoVMSM7feGGq";
+
+    //Struct for return values
+    $response = [
+        'success' => false,
+        'message' => 'Initial state',
+    ];
+
+    try{
+        $conn = new mysqli($host, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_errno) {
+            throw new Exception("Connection failed: " . $conn->connect_error);
+        }
+
+        //Terrain ID
+        $TerrainId = $conn->real_escape_string($_POST["NoiseDimensions"]);
+
+        $conn->begin_transaction();
+        try{
+
+            //Clear the asset table, as this ensures only current assets are stored
+            $DeleteAssets = $conn->prepare("DELETE FROM ManualAssets WHERE TerrainId = ?");
+            $DeleteAssets->bind_param("i", $TerrainId);
+            $DeleteAssets->execute();
+            
+            if (isset($_POST["Assets"])) {
+
+                $assets = json_decode($_POST["Assets"], true);
+                
+                if(is_array($assets)){
+                    $insertAsset = $conn->prepare("INSERT INTO ManualAssets (TerrainId, AssetId, xPos, yPos, zPos) VALUES (?,?,?,?,?) 
+                                                ON DUPLICATE KEY UPDATE TerrainId = LAST_INSERT_ID(TerrainId)");
+
+                    foreach($manAsset as $assets){
+                        $assetId = $manAsset["AssetId"];
+                        $xPos = $manAsset["xPos"];
+                        $yPos = $manAsset["yPos"];
+                        $zPos = $manAsset["zPos"];
+
+                        $insertAsset->bind_param("iiddd", $TerrainId, $assetId, $xPos, $yPos, $zPos);
+                        $insertAsset->execute();
+                    }
+                }
+            }
+
+            
+            $conn->commit();
+            $response['success'] = true;
+            $response['message'] = "Sucessfully Saved Terrain";
+            
+        } catch(Exception $e){
+            $conn->rollback();
+            throw $e;
+        }
+
+    } catch (Exception $e){
+        $response['message'] = "Error: " . $e->getMessage();
+    } finally {
+        if (isset($conn)){
+            $conn->close();
+        }
+        echo json_encode($response);
+    }
+   
+
+?>
+
